820ba06b30c970b8a7790db8ab38ab694b55295c
yeezick <ramerick5@gmail.com>
2021-12-22 20:34:25 -0500

    adding onClick to all sidebar links to close sidebar

--- src/layout/sidebar/Sidebar.jsx
+++ src/layout/sidebar/Sidebar.jsx
@@ -17,20 +17,18 @@ const Sidebar = ({ visibleSidebar, setVisibleSidebar }) => {
     >
       <IoIosClose size={50} onClick={handleSidebar} />
       <div className="sidebar_link_wrapper">
-        <div className="sidebar_link">
+        <div className="sidebar_link" onClick={handleSidebar}>
           <AiOutlineHome size={40} color="white" />
           <Link to="/">Home</Link>
         </div>
 
-        <div className="sidebar_link">
+        <div className="sidebar_link" onClick={handleSidebar}>
           <BsCheckSquare size={30} />
-
           <Link to="/completed">Completed</Link>
         </div>
 
-        <div className="sidebar_link">
+        <div className="sidebar_link" onClick={handleSidebar}>
           <BsListTask size={40} color="white" />
-
           <Link to="/all">All</Link>
         </div>
       </div>
